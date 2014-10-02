0a9df64599dbbe08184bd1f87971a66da06e1c63
Cloud User <ec2-user@ip-172-31-39-183.us-west-2.compute.internal>
2014-10-02 16:55:58 -0400

    Revert "okay"
    This reverts commit 668d5a96c3008dba201aba43a4bd6d83c7ade59a.

--- plannr/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-from plannr import views
-
-urlpatterns = patterns('',
-	url(r'^/?$', views.home, name="home"),
-)
--- plannr/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-	return render(request, 'plannr/home.html', {"title":"home"})
-
